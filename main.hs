5483eff4e0879f7c843861099bcc5c17209bcea5
RecursiveLogic <qrx9823@gmail.com>
2015-10-08 18:29:46 -0400

    Upload

---
+++ main.hs
@@ -0,0 +1,37 @@
+putTodo :: (Int, String) -> IO ()
+putTodo (n, todo) = putStrLn (show n ++ ": " ++ todo)
+
+prompt :: [String] -> IO ()
+prompt todos = do
+    putStrLn "\nCurrent To-do List:"
+    mapM_ putTodo (zip [0..] todos)
+    command <- getLine
+    getCommand command todos
+
+getCommand :: String -> [String] -> IO ()
+getCommand ('+':' ':todo) todos = prompt (todo:todos)
+getCommand ('-':' ':num) todos = 
+    case delete (read num) todos of
+        Nothing -> do
+            putStrLn "No To-do entry matches the given number"
+            prompt todos
+        Just todos' -> prompt todos'
+getCommand "Q"      todos = return ()
+getCommand command  todos = do
+    putStrLn ("Invalid command: '" ++ command ++ "'")
+    prompt todos
+
+delete :: Int -> [a] -> Maybe [a]
+delete 0 (_:as) = Just as
+delete n (a:as) = do
+    let x = n - 1
+    y <- x `seq` delete x as
+    return (a:y)
+delete _ [] = Nothing
+
+main = do
+    putStrLn "Commands:"
+    putStrLn "+ <String>  - Add a To-do entry"
+    putStrLn "- <Integer> - Delete the entry number"
+    putStrLn "Q           - Quit"
+    prompt []
